7e00affac45af6978f5e8c11beca691b386eaa90
Jeongwon, Bae <bjw020615@gmail.com>
2025-03-06 04:38:40 +0900

    dependency:

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   ignoreDuringBuilds: true,
 };
 
